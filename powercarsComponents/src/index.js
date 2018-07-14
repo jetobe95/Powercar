fc60825dfb71e650a7540746e875374a16feb8a3
Jeffrey <alto.951@hotmail.com>
2018-07-14 18:27:40 -0500

    limpiando

--- powercarsComponents/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import routes from '/home/danielmp96/powercars/src/components/global/config/routes.js';
-import registerServiceWorker from './registerServiceWorker.js';
-
-ReactDOM.render(routes,document.getElementById('root'));
-registerServiceWorker();
