ab442135642609f9f4782555e8d36f042cdef6f4
Jeffrey <alto.951@hotmail.com>
2018-07-29 16:15:20 -0500

    Copynf build

--- src/VARIABLES/PORTS.js
+++ src/VARIABLES/PORTS.js
@@ -2,7 +2,7 @@
 
 const PORTS = {
     API_PORT: "2999",
-    SERVER_IP_DEV:"http://172.20.10.2:2999",
+    SERVER_IP_DEV:"",
     SERVER_IP_PROD:"http://172.20.10.2:2999"
 }
 module.exports = PORTS;
---
+++ src/login/components/login.js
@@ -0,0 +1,10 @@
+class Login extends Component {
+    state = {  }
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Login
